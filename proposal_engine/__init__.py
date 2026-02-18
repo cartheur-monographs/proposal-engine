b62631819f2e3e544bc9016a67e522480d645bb2
C.A.Tucker <cartheur@pm.me>
2026-02-18 12:11:36 +0100

    Cleanup

--- proposal_engine/__init__.py
+++
@@ -1,4 +0,0 @@
-"""The Proposal Engine package."""
-
-__all__ = ["__version__"]
-__version__ = "0.1.0"
--- proposal_engine/__main__.py
+++
@@ -1,5 +0,0 @@
-from proposal_engine.cli import run
-
-
-if __name__ == "__main__":
-    raise SystemExit(run())
